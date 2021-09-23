4833e0ff37f31f4c0724b04b73f728459da58270
Rodrigo Borges <rborges.soares@gmail.com>
2021-09-23 18:37:18 -0300

    Fixed string extensions

--- Hereminders/Extensions/String+Extensions.swift
+++ Hereminders/Extensions/String+Extensions.swift
@@ -2,6 +2,6 @@ extension String {
 
     func removingSpaces() -> String {
 
-        return self.replacingOccurrences(of: " ", with: "_")
+        return self.replacingOccurrences(of: " ", with: "")
     }
 }
